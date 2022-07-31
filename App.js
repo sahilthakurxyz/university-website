6b0b22cc1ccff8b81b7cfd3d1a619943dbf50579
Sahil <sahilthakurxyz@gmail.com>
2022-07-31 11:59:55 +0530

    git setup for university

---
+++ App.js
@@ -0,0 +1,11 @@
+// ........JavaScript....... for toggle menu in Phone.......
+var navLinks = document.getElementById('navLinks');
+
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
